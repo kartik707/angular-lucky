30d3e9675af0bfb533593ac8262323a72a485bad
kartik707 <kartik933@live.com>
2019-06-21 21:05:25 +0530

    3 files modified

--- src/material/material.module.ts
+++ src/material/material.module.ts
@@ -1,12 +1,12 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { MatButtonModule, MatIconModule, MatFormFieldModule, MatInputModule } from '@angular/material'; 
+import { MatButtonModule, MatIconModule, MatFormFieldModule, MatInputModule,MatDatepickerModule, MatNativeDateModule } from '@angular/material'; 
 
 @NgModule({
   imports: [
-    CommonModule, MatButtonModule, MatIconModule, MatFormFieldModule, MatInputModule
+    CommonModule, MatButtonModule, MatIconModule, MatFormFieldModule, MatInputModule, MatDatepickerModule, MatNativeDateModule
   ],
   declarations: [],
-  exports: [MatButtonModule, MatIconModule,MatFormFieldModule, MatInputModule]
+  exports: [MatButtonModule, MatIconModule,MatFormFieldModule, MatInputModule, MatDatepickerModule, MatNativeDateModule]
 })
 export class MaterialModule { }
