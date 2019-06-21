d9b4c4b91319dfdc2b328faa994e5864e1058f20
kartik707 <kartik933@live.com>
2019-06-21 13:57:08 +0530

    2 files modified

--- src/material/material.module.ts
+++ src/material/material.module.ts
@@ -1,12 +1,12 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { MatButtonModule, MatIconModule } from '@angular/material'; 
+import { MatButtonModule, MatIconModule, MatFormFieldModule, MatInputModule } from '@angular/material'; 
 
 @NgModule({
   imports: [
-    CommonModule, MatButtonModule, MatIconModule
+    CommonModule, MatButtonModule, MatIconModule, MatFormFieldModule, MatInputModule
   ],
   declarations: [],
-  exports: [MatButtonModule, MatIconModule]
+  exports: [MatButtonModule, MatIconModule,MatFormFieldModule, MatInputModule]
 })
 export class MaterialModule { }
